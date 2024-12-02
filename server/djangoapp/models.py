0f202d1efcbda24bb98f3edccf3ab12a1bbc1bb0
Daniels Satcs <shatz.dan@gmail.com>
2024-12-02 13:43:41 -0500

    Fix lint warnings

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
-
 
 
 class CarMake(models.Model):
@@ -24,10 +22,11 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+                               validators=[
+                                   MaxValueValidator(2023),
+                                   MinValueValidator(2015)
+                               ])
+
     # Other fields as needed
 
     def __str__(self):
